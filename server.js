24ac66a0004eb40af5d324dd4463f759492bbf83
NoorunnisaSulthan <balssanoor27@gmail.com>
2025-04-02 12:34:14 +0400

    value change

--- server.js
+++ server.js
@@ -34,11 +34,14 @@ app.post('/ask', async (req, res) => {
       // Azure AI Search API request
       const response = await axios.post(
           `${AZURE_SEARCH_ENDPOINT}/indexes/${INDEX_NAME}/docs/search?api-version=2024-07-01`,
-          { search: userQuery,top:2, select: "title, content" },
+          { search: userQuery,top:1, select: "title, content" },
           { headers: { 'Content-Type': 'application/json', 'api-key': AZURE_SEARCH_API_KEY } }
       );
 
-      const results = response.data.value.map(item => `${item.content}\n \n`).join("\n\n");
+      const results = response.data["@search.answers"]
+      ? response.data["@search.answers"][0].text + "\n\n"
+      : response.data.value.map(item => `${item.content}\n\n`).join("\n\n");
+  
 
       res.json({ response: results || "No relevant results found." });
   } catch (error) {
